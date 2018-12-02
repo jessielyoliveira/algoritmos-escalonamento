249ce30159a295ab225081a5eab47cc03b5b9d31
Jessiely Oliveira <jessiely@imd.ufrn.br>
2018-12-02 15:30:57 -0300

    algoritmo SSTF

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import sys
 
 # Algoritmo FCFS
-def FCFS(fila, cabecote):
-	fila.insert(0, cabecote)
-
+def FCFS(fila):
 	atendidos = []
 	cilindros = 0
 
@@ -17,7 +15,45 @@ def FCFS(fila, cabecote):
 
 	print('\nFCFS')
 	print('     Ordem: ', atendidos)
-	print('     Cilindros: ', cilindros)
+	print('     Cilindros: ', cilindros, '\n')
+
+
+
+# Algoritmo SSTF
+def SSTF(fila):
+	cilindros = 0
+	cabeca = fila.pop(0)
+	atendidos = [cabeca]
+	SSTF_help(fila, cabeca, atendidos, cilindros)
+
+def SSTF_help(fila, cabeca, atendidos, cilindros):
+	if len(fila) == 0:
+		atendidos = ', '.join(atendidos)
+
+		print('SSTF')
+		print('     Ordem: ', atendidos)
+		print('     Cilindros: ', cilindros, '\n')
+		return
+	else:
+		pos_menor = menor_diferenca(fila, cabeca)
+		atendidos.append(fila[pos_menor])
+		atual = fila.pop(pos_menor)
+
+		c = abs(int(atual) - int(cabeca))
+		cilindros += c
+
+		SSTF_help(fila, atual, atendidos, cilindros)
+		
+def menor_diferenca(fila, cabeca):
+	diff = []
+	for f in fila:
+		diff.append(abs(int(f) - int(cabeca))) 
+	menor = min(diff)
+	return diff.index(menor)
+
+
+
+
 
 def main():
 	# fila_espera = []
@@ -32,12 +68,14 @@ def main():
 			cabecote = arquivo.readline().replace('\n', '')	# Posição inicial do cabeçote
 			tam_fila = arquivo.readline().replace('\n', '')	# Tamanho da fila de espera
 			fila = arquivo.readline().split()		# Fila de espera
+			fila.insert(0, cabecote)
 			arquivo.close()
 	except FileNotFoundError:
 		print("arquivo não encontrado")
 		return
 
-	FCFS(fila, cabecote)
+	FCFS(fila)
+	SSTF(fila)
 
 if __name__ == '__main__':
 		main()
